adf2d4f7ad2557d763ce98b070a4eb0386996d52
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-11-11 12:33:46 +0900

    add snapshot test

--- apps/web/src/components/user-profile/user-profile.test.tsx
+++ apps/web/src/components/user-profile/user-profile.test.tsx
@@ -2,6 +2,12 @@ import { UserProfile } from './user-profile';
 import { render } from '@testing-library/react';
 
 describe('UserProfile', () => {
+  it('snapshot unchanged', () => {
+    const { asFragment } = render(<UserProfile userName='John Doe' />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it('ユーザー名を表示すること', () => {
     const { getByText } = render(<UserProfile userName='John Doe' />);
 
